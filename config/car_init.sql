3a0fa65caad73ac1ab8d9d96c3a6fd9c94d69894
SwiLimes <nikitos15052001@gmail.com>
2023-05-01 15:00:16 +0300

    sql init files

---
+++ config/car_init.sql
@@ -0,0 +1,16 @@
+insert into brand (brand_name, model_name)
+values ('BMW', 'X5'),
+       ('AUDI', 'Q7'),
+       ('SKODA', 'Superb'),
+       ('KIA', 'K5');
+
+
+insert into engine (type, capacity, power, fuel_type)
+values ('Турбированный', 2.5, 220, 'Дизель'),
+       ('Атмосферный', 1.8, 180, 'Бензин');
+
+insert into car (vin, brand_name, model_name, car_year, car_type, equipment, engine_id, car_body, drive_unit,
+                 transmission_id, consumption, clearance)
+values ('QWERTYUIOPASDFGHJ', 'SKODA', 'Superb', 2014, 'SEDAN', 'top', 1, 'Car_body',
+        'drive_unit', 'transmission', 8.5, 17);
+
