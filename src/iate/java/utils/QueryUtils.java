9d896e60f853291d17cccb037dbb86c20e38e8e7
SwiLimes <nikitos15052001@gmail.com>
2023-05-29 20:38:41 +0300

    Changed year filter to year range

--- src/iate/java/utils/QueryUtils.java
+++ src/iate/java/utils/QueryUtils.java
@@ -15,7 +15,13 @@ public class QueryUtils {
     public static String buildCarFilterQuery(String searchQuery, Map<CarFilterCriterion, String> params) {
         return searchQuery + params.entrySet().stream()
                 .filter(entry -> !entry.getValue().isEmpty())
-                .map(entry -> String.format("%s = '%s'", entry.getKey().getSqlName(), entry.getValue()))
+                .map(entry ->
+                    switch (entry.getKey()) {
+                        case YEAR_FROM -> String.format("car_year >= %d", Integer.parseInt(entry.getValue()));
+                        case YEAR_TO -> String.format("car_year <= %d", Integer.parseInt(entry.getValue()));
+                        default -> String.format("%s = '%s'", entry.getKey().getSqlName(), entry.getValue());
+                    }
+                )
                 .collect(Collectors.joining(" AND ")) + ";";
     }
 }
