b17135650685d27e8179e82a443780d413b7224f
semenmyslin <110462243+semenmyslin@users.noreply.github.com>
2023-05-31 15:17:51 +0300

    done

--- src/iate/java/utils/CarFilterCriterion.java
+++ src/iate/java/utils/CarFilterCriterion.java
@@ -4,7 +4,8 @@ public enum CarFilterCriterion {
     VIN("vin"),
     BRAND("brand_name"),
     MODEL("model_name"),
-    YEAR("car_year");
+    YEAR_FROM("year_from"),
+    YEAR_TO("year_to");
 
     private final String sqlName;
 
--- src/iate/java/utils/QueryUtils.java
+++ src/iate/java/utils/QueryUtils.java
@@ -9,13 +9,20 @@ public class QueryUtils {
 
     /**
      * Построение запроса
+     *
      * @param searchQuery запрос в бд
-     * @param params параметры для поиска
+     * @param params      параметры для поиска
      */
     public static String buildCarFilterQuery(String searchQuery, Map<CarFilterCriterion, String> params) {
         return searchQuery + params.entrySet().stream()
                 .filter(entry -> !entry.getValue().isEmpty())
-                .map(entry -> String.format("%s = '%s'", entry.getKey().getSqlName(), entry.getValue()))
+                .map(entry ->
+                        switch (entry.getKey()) {
+                            case YEAR_FROM -> String.format("car_year >= %d", Integer.parseInt(entry.getValue()));
+                            case YEAR_TO -> String.format("car_year <= %d", Integer.parseInt(entry.getValue()));
+                            default -> String.format("%s = '%s'", entry.getKey().getSqlName(), entry.getValue());
+                        }
+                )
                 .collect(Collectors.joining(" AND ")) + ";";
     }
 }
